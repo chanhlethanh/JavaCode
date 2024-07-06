62caae18465b53f1e4ee5e50d9878f309c8a0030
ChanhFX <chanhltfx28124@funix.edu.vn>
2024-07-06 20:13:05 +0700

    The Fourth Java code

--- src/Lesson2/NewBasicHandleCode.java
+++ src/Lesson2/NewBasicHandleCode.java
@@ -1,5 +1,7 @@
 package Lesson2;
 
+import java.util.Scanner;
+
 public class NewBasicHandleCode {
     public static void main(String[] args) {
 //"And" Operator
@@ -7,6 +9,80 @@ public class NewBasicHandleCode {
         boolean isWarm = temperature > 20 && temperature < 30;
         System.out.println(isWarm);
 //"Or" Operator
+        boolean hasHighIncome = false;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+        System.out.println(isEligible);
+//If Statements
+        int temp = 32;
+        if (temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        } else if (temp > 20)
+            System.out.println("Beautiful day");
+        else
+            System.out.println("Cold day");
+//Simplifying If Statements
+//        int income = 120_000;
+//        boolean hasHighIncomex = (income > 100_000);
+//The Ternary Operator
+        int income = 120_000;
+        String className = income > 100_000 ? "Economy" : "First";
+//Switch Statements
+        String role = "admin";
+        switch (role) {
+            case "admin":
+                System.out.println("You're an admin");
+                break;
+            case "moderator":
+                System.out.println("You're an moderator");
+                break;
+            default:
+                System.out.println("You're a guest");
+        }
+//Excercise: FizzBuzz
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+        if (number % 5 == 0) {
+            if (number % 3 == 0)
+                System.out.println("FizzBuzz");
+            else
+                System.out.println("Fizz");
+        } else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else
+            System.out.println(number);
+//For Loops
+        for (int i = 0; i < 5; i++)
+            System.out.println("Hello World " + i);
+//While Loops
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            System.out.println("Input: ");
+            input = sc.next().toLowerCase();
+            if (input.equals("pass"))
+                continue;
+            if (input.equals("quit"))
+                break;
+            System.out.println(input);
+        }
+//Do...While Loops
+//        Scanner sc = new Scanner(System.in);
+//        String input = "";
+//        do {
+//            System.out.println("Input: ");
+//            input = sc.next().toLowerCase();
+//            System.out.println(input);
+//        } while (!input.equals("quit"));
+//For-Each Loop
+//In Constrast
+        String[] fruits = {"Apple", "Mango", "Orange"};
+//        for (int i = fruits.length ; i > 0; i--)
+//            System.out.println(fruits[i]);
+        for (String fruit : fruits)
+            System.out.println(fruit);
     }
-
 }
--- src/Project/MortageCalculator/SolutionMortgageCalculator.java
+++ src/Project/MortageCalculator/SolutionMortgageCalculator.java
@@ -9,21 +9,44 @@ public class SolutionMortgageCalculator {
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
+        int principal;
+        float annualInterest;
+        float monthlyInterest;
+        int numberOfPayments;
+        byte years;
 
-        System.out.println("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.println("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1000 and 1000000");
+        }
 
-        System.out.println("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
 
-        System.out.println("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTH_IN_YEAR;
+        while (true) {
+            System.out.println("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+
+        while (true) {
+            System.out.println("Period (Years): ");
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTH_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
         double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
-        
+
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Morgage: " + mortgageFormatted);
     }
